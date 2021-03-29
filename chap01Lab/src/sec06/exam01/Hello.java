3b03fb2f077a71dedba6900f7f61d944d09cc843
Administrator <Administrator@424-18>
2021-03-29 11:08:44 +0900

    chap01 초기 버전

---
+++ chap01Lab/src/sec06/exam01/Hello.java
@@ -0,0 +1,18 @@
+package sec06.exam01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//표준입출력(콘솔)에 "Hello, Java!" 문자열을 출력한다.
+		System.out.println("1. 이름 : 이정건");
+		System.out.println("2. 전화번호 : 010-7727-9390");
+		System.out.println("3. 희망사항 : 개발자로 취업");
+		/*
+		 * 1. (4)		2. (3)		3. (3 - 1 - 2 - 4)
+		 * 4. (4)		5. (4)		6. (4)
+		 * */
+
+	}
+
+}
